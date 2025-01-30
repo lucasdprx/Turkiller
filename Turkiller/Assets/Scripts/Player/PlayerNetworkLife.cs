7e7513479211b8d5558751f75ba3358cd58bee61
Zakaria "LeMASHUP" Dutertre <dutertre.zakaria@gmail.com>
2025-01-30 16:40:11 +0100

    Network Rework + Slider Rework

---
+++ Turkiller/Assets/Scripts/Player/PlayerNetworkLife.cs
@@ -0,0 +1,52 @@
+using NUnit.Framework;
+using UnityEngine;
+using Unity.Netcode;
+using UnityEditor.UI;
+using UnityEngine.UI;
+
+public class PlayerNetworkLife : NetworkBehaviour
+{
+    [SerializeField] private Slider _lifeSlider;
+    [SerializeField] private NetworkVariable<int> _maxLife = new NetworkVariable<int>(100);
+    private NetworkVariable<float> _currentLife = new NetworkVariable<float>(100);
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsServer)
+        {
+            _currentLife.Value = _maxLife.Value;
+            _lifeSlider.maxValue = _maxLife.Value;
+            _lifeSlider.value = _currentLife.Value;
+        }
+    }
+
+    [ServerRpc]
+    public void TakeDamageServerRpc(int damage)
+    {
+        _currentLife.Value -= damage;
+        _lifeSlider.value = _currentLife.Value;
+
+        if (_currentLife.Value <= 0)
+        {
+            Die();
+        }
+    }
+    
+    [ServerRpc]
+    public void HealServerRpc(int heal)
+    {
+        _currentLife.Value += heal;
+        _lifeSlider.value = _currentLife.Value;
+    }
+
+    public void Die()
+    {
+        if (!IsServer) return;
+        Debug.Log("you are dead");
+    }
+    
+    public bool IsAlive()
+    {
+        return _currentLife.Value > 0;
+    }
+}
--- Turkiller/Assets/Scripts/Player/ProjectileComponent.cs
+++ Turkiller/Assets/Scripts/Player/ProjectileComponent.cs
@@ -6,22 +6,37 @@ public class ProjectileComponent : NetworkBehaviour
     [SerializeField] private float _speed = 15f;
     [SerializeField] private float _distance = 10f;
     private float _distanceTraveled;
-    
 
     private void Update()
     {
         if (!IsServer) return;
-        
+
         transform.position += transform.right * (_speed * Time.deltaTime);
         _distanceTraveled += _speed * Time.deltaTime;
 
-        if (!(_distanceTraveled >= _distance))
-            return;
-        
-        NetworkObject networkObject = gameObject.GetComponent<NetworkObject>();
-        if (networkObject != null)
+        if (_distanceTraveled >= _distance)
         {
-            networkObject.Despawn(true);
+            NetworkObject networkObject = gameObject.GetComponent<NetworkObject>();
+            if (networkObject != null)
+            {
+                networkObject.Despawn(true);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!IsServer) return;
+
+        PlayerNetworkLife playerNetworkLife = collision.gameObject.GetComponent<PlayerNetworkLife>();
+        if (playerNetworkLife != null)
+        {
+            playerNetworkLife.TakeDamageServerRpc(10); // Apply 10 points of damage
+            NetworkObject networkObject = gameObject.GetComponent<NetworkObject>();
+            if (networkObject != null)
+            {
+                networkObject.Despawn(true);
+            }
         }
     }
 }

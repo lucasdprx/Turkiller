c597c3f1f7ca66f4f2334648ce1581f7147ee89f
Hana <xyzmisterysolve@gmail.com>
2025-02-07 14:30:04 +0100

    ui connexion + ui in-game(prefab)

---
+++ Turkiller/Assets/UI/Sign_up_script.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class Sign_up_script : UI
+{
+
+}
---
+++ Turkiller/Assets/UI/settings_script.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class settings_script : UI
+{
+    public void ChangeVolume()
+    {
+        
+    }
+}
